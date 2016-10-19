6b456c362b8156a33024ce8a7834330fbe3d3fdf
Adrian Kalaszi <kalaszi.adrian@gmail.com>
2016-10-19 14:58:07 +0200

    fix test

--- debugaspect/test.sh
+++ debugaspect/test.sh
@@ -1,10 +1,11 @@
 set -e -x
-testfile=filesystem-test-env-1.0-SNAPSHOT.jar
+testfile=amshare-test-env-1.0-SNAPSHOT.jar
 dot=`pwd`
 cd ../testenvironment
 mvn clean package
 cp target/$testfile $dot
 cd $dot
 rm -f aspect.jar
-ajc -cp $testfile:aspectjrt.jar -outjar aspect.jar src/aspects/*.aj
-jar uf aspect.jar META-INF/;java -javaagent:aspectjweaver.jar -classpath "aspect.jar:aspectjrt.jar:$testfile" com.cloudera.impala.common.FileSystemUtil
+ajc -cp $testfile:aspectjrt.jar -outjar aspect.jar src/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSLeafQueueAspect.aj
+jar uf aspect.jar META-INF/
+java -javaagent:aspectjweaver.jar -classpath "aspect.jar:aspectjrt.jar:$testfile" org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FSAppAttempt
